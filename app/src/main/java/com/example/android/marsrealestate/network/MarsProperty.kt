ee98dcc7c34e29c2be3749b1811d3ff7ea8cd4a6
fwornle <fwornle@yahoo.co.uk>
2021-09-21 19:20:24 +0200

    Added support for Parceliz-ation

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -18,10 +18,13 @@
 package com.example.android.marsrealestate.network
 
 import com.squareup.moshi.Json
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class MarsProperty(
     val id: String,
     @Json(name = "img_src") val imgSrcUrl: String,
     val type: String,
     val price: Double,
-)
+) : Parcelable
